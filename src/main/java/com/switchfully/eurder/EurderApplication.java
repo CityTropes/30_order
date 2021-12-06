b5bd17ee7195a5458bae5c0f3ac99962013ba1c6
Jan De Pelsmacker <jan.depelsmacker@gmail.com>
2021-12-06 10:18:00 +0100

    init commit: start project Eurder

---
+++ src/main/java/com/switchfully/eurder/EurderApplication.java
@@ -0,0 +1,17 @@
+package com.switchfully.eurder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurderApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurderApplication.class, args);
+
+
+        System.out.println("ending main...");
+    }
+
+}
